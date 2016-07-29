0200af20265a27db70410c910969be40cf615fe7
RGunning <rjgunning@gmail.com>
2016-07-29 19:05:32 +0100

    reformat and restructure

--- config.py
+++ config.py
@@ -1,37 +1,38 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-#urls
-api_url='https://pgorelease.nianticlabs.com/plfe/rpc'
-login_url='https://sso.pokemon.com/sso/oauth2.0/authorize?client_id=mobile-app_pokemon-go&redirect_uri=https%3A%2F%2Fwww.nianticlabs.com%2Fpokemongo%2Ferror'
-login_oauth='https://sso.pokemon.com/sso/oauth2.0/accessToken'
-#urls end
+# urls
+api_url = 'https://pgorelease.nianticlabs.com/plfe/rpc'
+login_url = 'https://sso.pokemon.com/sso/oauth2.0/authorize?client_id=mobile-app_pokemon-go&redirect_uri=https%3A%2F%2Fwww.nianticlabs.com%2Fpokemongo%2Ferror'
+login_oauth = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
+# urls end
 
-#values
-use_proxy=False
-debug=False
-#distance=0
-steps=0.000095
-google=True
-pub=None
-earned_xp=0
-use_powerball=False
-#values end
+# values
+use_proxy = False
+debug = False
+# distance=0
+steps = 0.000095
+google = True
+pub = None
+earned_xp = 0
+use_powerball = False
+# values end
 
-#session
+# session
 proxies = {
-  'http': 'http://127.0.0.1:8888',
-  'https': 'http://127.0.0.1:8888',
+    'http': 'http://127.0.0.1:8888',
+    'https': 'http://127.0.0.1:8888',
 }
-s=requests.session()
+s = requests.session()
 if use_proxy:
-	s.proxies.update(proxies)
-	s.verify=False
-s.headers.update({'User-Agent':'Niantic App'})
-#session end
+    s.proxies.update(proxies)
+    s.verify = False
+s.headers.update({'User-Agent': 'Niantic App'})
+# session end
 ###########################################################################################
-#public
+# public
 API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
 LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
 LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
@@ -40,7 +41,7 @@ PTC_CLIENT_SECRET = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2w
 SESSION = requests.session()
 SESSION.headers.update({'User-Agent': 'Niantic App'})
 if use_proxy:
-	SESSION.proxies.update(proxies)
-	SESSION.verify = False
+    SESSION.proxies.update(proxies)
+    SESSION.verify = False
 DEBUG = True
 ###########################################################################################
